e42711c5474525b922db2530e9c879e5600178f7
Vikas Mahana <maharanavikas2@gmail.com>
2026-02-20 22:45:48 +0530

    changes

--- Array/Array Partition.py
+++ Array/Array Partition.py
@@ -4,6 +4,9 @@ group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
 such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 """
 
+from typing import List
+
+
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
