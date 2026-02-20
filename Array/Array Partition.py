e7ffd6ef9b41ca5f1328a6d8d0e8a1f99028133a
Vikas Mahana <maharanavikas2@gmail.com>
2026-02-20 23:18:51 +0530

    changes

--- Array/Array Partition.py
+++ Array/Array Partition.py
@@ -5,8 +5,6 @@ such that the sum of min(ai, bi) for all i is maximized. Return the maximized su
 """
 
 from typing import List
-
-
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
